ea475476c3e8e9ef71ce8fdcfa8a2ba509895b1b
Kyle <KyleBurton360@gmail.com>
2022-01-22 13:05:26 -0800

    new branch for arduino code

--- ArduinoComms/include/robot.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ROBOT_H_
-#define _ROBOT_H_
-
-#include "main.h"
-
-#define ARDUINO_PORT 5
-#define ARDUINO_BAUDRATE 115200
-
-#endif
---
+++ arduino_test.ino
@@ -0,0 +1,30 @@
+#define encoder1A 3
+#define encoder1B 2
+#define encoder1Out 11
+#define ppr 100
+
+int countTick = 0;
+int tickA = 0;
+int tickB =0;
+
+void setup() {
+  pinMode(encoder1A, INPUT);
+  pinMode(encoder1B, INPUT);
+  pinMode(encoder1Out, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(encoder1A), computeRotation, RISING);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(countTick);
+  analogWrite(encoder1Out, countTick);
+  delay(500);
+}
+
+void computeRotation() {
+  if (digitalRead(encoder1B)) {
+    countTick += 1;
+  } else {
+    countTick -= 1;
+  }
+}
