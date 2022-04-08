cfabd76493b62d578ea0a5876de05669ec335a34
Miguel Saenz <saenzm4307@gmail.com>
2022-04-07 17:08:20 -0700

    Update opcontrol.cpp

--- vex/largeRobot/src/atreides/opcontrol.cpp
+++ vex/largeRobot/src/atreides/opcontrol.cpp
@@ -26,17 +26,18 @@ void at_opcontrol_update() {
     at_clawDeployMotor.stepRetract();
   }
 
-  if (at_btnArmDown.isPressed()) {
-    Arm1.move_voltage(12000);
-    Arm2.move_voltage(-12000);
-  } else if (at_btnArmUp.isPressed()) {
-    Arm1.move_voltage(-12000);
-    Arm2.move_voltage(12000);
-  }
-  else{
-      Arm1.move_voltage(0);
-      Arm2.move_voltage(0);
-  }
+	// arm lifting has been commented out
+  //if (at_btnArmDown.isPressed()) {
+  //  Arm1.move_voltage(12000);
+  //  Arm2.move_voltage(-12000);
+  //} else if (at_btnArmUp.isPressed()) {
+  //  Arm1.move_voltage(-12000);
+   // Arm2.move_voltage(12000);
+  //}
+  //else{
+    //  Arm1.move_voltage(0);
+    //  Arm2.move_voltage(0);
+  //}
 
   if (at_btnClaw.changedToPressed()) {
 		at_clawClosed = !at_clawClosed;
